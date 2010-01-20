3d243f9307612706adb4219b26e5a89bdca6c2e8
Christopher Farrow <farrowch@gmail.com>
2010-01-20 21:12:38 +0000

    Adding SAS example.

---
+++ doc/examples/ellipsoidsas.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+########################################################################
+#
+# diffpy.srfit      by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2008 Trustees of the Columbia University
+#                   in the City of New York.  All rights reserved.
+#
+# File coded by:    Chris Farrow
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+########################################################################
+"""Example of a SAS refinement of ellipsoidal I(Q) data.
+
+"""
+
+import os
+
+import numpy
+
+from diffpy.srfit.sas import SASGenerator, SASParser
+from diffpy.srfit.fitbase import FitContribution, FitRecipe
+from diffpy.srfit.fitbase import FitResults, Profile
+
+from gaussianrecipe import scipyOptimize
+
+
+
+####### Example Code
+
+def makeRecipe(datname):
+    """Create a fitting recipe for ellipsoidal SAS data."""
+
+    ## The Profile
+    # This will be used to store the observed and calculated I(Q) data.
+    profile = Profile()
+
+    # Load data and add it to the Profile. We use a SASParser to load the data
+    # properly and pass the metadata along.
+    parser = SASParser()
+    parser.parseFile(datname)
+    profile.loadParsedData(parser)
+
+    ## The ProfileGenerator
+    # The SASGenerator is for configuring and calculating a SAS profile. We use
+    # a configurable sans model to configure the generator. This allows us to
+    # use the full sans model creation capabilities, and tie this into SrFit
+    # when we want to fit a model to data.
+    from sans.models.EllipsoidModel import EllipsoidModel
+    model = EllipsoidModel()
+    generator = SASGenerator("generator", model)
+
+    ## The FitContribution
+    # Here we associate the Profile and ProfileGenerator, as has been done
+    # before. 
+    contribution = FitContribution("ellipsoid")
+    contribution.addProfileGenerator(generator)
+    contribution.setProfile(profile, xname = "r")
+
+    # We want to fit the log of the signal to the log of the data so that the
+    # higher-Q information remains significant. There are no I(Q) uncertainty
+    # values with the data, so we do not need to worry about the effect this
+    # will have on the estimated parameter uncertainties.
+    contribution.setResidualEquation("(log(eq) - log(y))/dy")
+
+    ## Make the FitRecipe and add the FitContribution.
+    recipe = FitRecipe()
+    recipe.addContribution(contribution)
+
+    ## Configure the fit variables
+    # The SASGenerator uses the parameters from the params and dispersion
+    # attribues of the model. These vary from model to model, but are adopted
+    # as SrFit Parameters within the generator. Whereas the dispersion
+    # parameters are accessible as, e.g. "radius.width", within the
+    # SASGenerator these are named like "radiuswidth".
+    #
+    # We want to fit the scale factor, radii and background factors.
+    recipe.addVar(generator.scale, 1)
+    recipe.addVar(generator.radius_a, 50)
+    recipe.addVar(generator.radius_b, 500)
+    recipe.addVar(generator.background, 0)
+    
+    # Give the recipe away so it can be used!
+    return recipe
+
+def plotResults(recipe):
+    """Plot the results contained within a refined FitRecipe."""
+
+    # All this should be pretty familiar by now.
+    names = recipe.getNames()
+    vals = recipe.getValues()
+
+    r = recipe.ellipsoid.profile.x
+
+    y = recipe.ellipsoid.profile.y
+    ycalc = recipe.ellipsoid.profile.ycalc
+    diff = y - ycalc - 0.8 * max(y)
+
+    import pylab
+    pylab.loglog(r,y,'bo',label="I(Q) Data")
+    pylab.loglog(r, ycalc,'r-',label="I(Q) Fit")
+    pylab.loglog(r,diff,'g-',label="I(Q) diff")
+    pylab.xlabel("$Q (\AA^{-1})$")
+    pylab.ylabel("$I (arb. units)$")
+    pylab.legend(loc=1)
+
+    pylab.show()
+    return
+
+if __name__ == "__main__":
+
+    # Make the data and the recipe
+    data = "data/sas_ellipsoid_testdata.txt"
+
+    # Make the recipe
+    recipe = makeRecipe(data)
+
+    # Optimize
+    scipyOptimize(recipe)
+
+    # Generate and print the FitResults
+    res = FitResults(recipe)
+    res.printResults()
+
+    # Plot!
+    plotResults(recipe)
+
+# End of file

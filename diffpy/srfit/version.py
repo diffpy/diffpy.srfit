b38ab2591518d1dc43952bb68a88f30b0253a676
Pavol Juhas <pavol.juhas@gmail.com>
2013-08-05 14:59:54 -0400

    Resolve version from git tag.
    Version tagging is now done from git-describe.
    Add __gitsha__ variable to the version.py script.

--- diffpy/srfit/version.py
+++ diffpy/srfit/version.py
@@ -15,11 +15,8 @@
 
 
 """Definition of __version__ and __date__ for diffpy.srfit .
-
 """
 
-# module version
-__id__ = "$Id$"
 
 # obtain version information
 from pkg_resources import get_distribution
@@ -28,6 +25,9 @@ __version__ = get_distribution('diffpy.srfit ').version
 # we assume that tag_date was used and __version__ ends in YYYYMMDD
 __date__ = __version__[-8:-4] + '-' + \
            __version__[-4:-2] + '-' + __version__[-2:]
+
+# GIT SHA hash is the second last component in the version string
+__gitsha__ = __version__.rsplit('-', 2)[-2]
 
 
 # End of file

58c483aee77dbbe556bcc23636f0cd83d9033953
Eric Dill <edill@bnl.gov>
2015-08-06 16:28:26 -0400

    PY3: cStringIO -> io

--- diffpy/srfit/equation/builder.py
+++ diffpy/srfit/equation/builder.py
@@ -308,9 +308,13 @@ class EquationFactory(object):
         """
         import tokenize
         import token
-        import cStringIO
+        try:
+            from cStringIO import StringIO
+        except ImportError:
+            # python 3 does not have cStringIO
+            from io import StringIO
 
-        interface = cStringIO.StringIO(eqstr).readline
+        interface = StringIO(eqstr).readline
         # output is an iterator. Each entry (token) is a 5-tuple
         # token[0] = token type
         # token[1] = token string

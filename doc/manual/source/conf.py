b2fd39c2c55420f7ff75e566339a94f36db9900b
Pavol Juhas <pavol.juhas@gmail.com>
2016-12-07 18:59:48 -0500

    Include micro field in sphinx version.
    Just strip away anything after the ".post" marker.

--- doc/manual/source/conf.py
+++ doc/manual/source/conf.py
@@ -57,7 +57,7 @@ sys.path.insert(0, os.path.abspath('../../..'))
 from setup import versiondata
 fullversion = versiondata.get('DEFAULT', 'version')
 # The short X.Y version.
-version = '.'.join(fullversion.split('.')[:2])
+version = ''.join(fullversion.split('.post')[:1])
 # The full version, including alpha/beta/rc tags.
 release = fullversion
 

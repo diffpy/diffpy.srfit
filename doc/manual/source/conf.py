64f15bf071b204906d34463e1ef42ddcbcf26f24
Pavol Juhas <pavol.juhas@gmail.com>
2016-12-07 18:51:18 -0500

    Activate the napoleon extension for sphinx.
    Support parsing of humanly marked-up docstrings.
    Remove the unused ifconfig extension.

--- doc/manual/source/conf.py
+++ doc/manual/source/conf.py
@@ -31,7 +31,8 @@ ab_authors = u'Christopher L. Farrow, Pavol Juhás, Simon J.L. Billinge group'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.ifconfig', 'sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

9c05356d915546afa52098b3ec101d3a13b6a017
Pavol Juhas <pavol.juhas@gmail.com>
2017-03-11 00:47:00 -0500

    Convert travis tests for Python 3.
    - test Python versions 3.4, 3.5, 3.6.
    - Test non-conda version with homebrew on Mac OS
      (Ignore linux apt python 3 which is very old).

--- setup.py
+++ setup.py
@@ -102,7 +102,6 @@ setup_args = dict(
         'License :: OSI Approved :: BSD License',
         'Operating System :: MacOS',
         'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',

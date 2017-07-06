c1611f36f32333f77eca0588ae57d374f8b3579e
Pavol Juhas <pavol.juhas@gmail.com>
2017-07-06 17:32:44 -0400

    Declare namespace package with pkgutil module.
    Use the builtin module instead of pkg_resources from setuptools.
    This is compatible with PEP 420 namespace packages in Python 3.

--- src/diffpy/__init__.py
+++ src/diffpy/__init__.py
@@ -16,10 +16,12 @@
 
 """diffpy - tools for structure analysis by diffraction.
 
-This is the namespace package shared by all DiffPy software.
+Blank namespace package.
 """
 
-__import__('pkg_resources').declare_namespace(__name__)
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
 
 
 # End of file

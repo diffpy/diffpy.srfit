40c1de1ce0849899020702f4442db726b51b311c
Tieqiong Zhang <tz2600@columbia.edu>
2024-07-02 13:13:38 -0400

    diffpy/__init__, srfit/__init__, tests/debug, run

--- src/diffpy/srfit/tests/debug.py
+++ src/diffpy/srfit/tests/debug.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# diffpy.srfit      Complex Modeling Initiative
-#                   (c) 2016 Brookhaven Science Associates,
-#                   Brookhaven National Laboratory.
-#                   All rights reserved.
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
 #
-# File coded by:    Pavol Juhas
+# File coded by: Billinge Group members and community contributors.
 #
-# See AUTHORS.txt for a list of people who contributed.
-# See LICENSE.txt for license information.
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/diffpy/diffpy.srfit/graphs/contributors
+#
+# See LICENSE.rst for license information.
 #
 ##############################################################################
 
@@ -22,10 +22,12 @@ Exceptions raised by failed tests or other errors are not caught.
 """
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     from diffpy.srfit.tests import testsuite
-    pattern = sys.argv[1] if len(sys.argv) > 1 else ''
+
+    pattern = sys.argv[1] if len(sys.argv) > 1 else ""
     suite = testsuite(pattern)
     suite.debug()
 

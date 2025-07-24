aa3cdb4fa44e901abb19a29027378f246b4ca687
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-07-24 19:16:55 -0400

    style: use `CODE-OF-CONDUCT.rst`, `tests.txt` and `docs`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx_rtd_theme",
     "sphinx_copybutton",
-    "m2r",
+    "m2r2",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -141,7 +141,7 @@ html_context = {
     "github_user": "diffpy",
     "github_repo": "diffpy.srfit",
     "github_version": "main",
-    "conf_py_path": "/doc/source/",
+    "conf_py_path": "/docs/source/",
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
---
+++ news/license.rst
@@ -0,0 +1,24 @@
+**Added:**
+
+* <news item>
+
+**Changed:**
+
+* doc/ -> docs/
+* requirements/test.txt -> requirements/tests.txt
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* <news item>
+
+**Fixed:**
+
+* Better license designation in `pyproject.toml`
+
+**Security:**
+
+* <news item>

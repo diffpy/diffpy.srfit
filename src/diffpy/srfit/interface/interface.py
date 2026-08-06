4b88be3bf904ad4a258a31ed9b7454fcb5e776f4
Caden Myers <cjm2304@columbia.edu>
2026-08-06 11:01:29 -0400

    update docstrings in interface and structure to group standards

--- src/diffpy/srfit/interface/interface.py
+++ src/diffpy/srfit/interface/interface.py
@@ -36,14 +36,19 @@ class ParameterInterface(object):
     """Mix-in class for enhancing the Parameter interface."""
 
     def __lshift__(self, v):
-        """set_value with <<
+        """Set the value with ``<<``.
 
-        Think of '<<' as injecting a value
+        Think of ``<<`` as injecting a value.
 
-        Attributes
+        Parameters
         ----------
-        v
-            value or Argument derivative
+        v : float or ArgumentABC
+            The value, or Argument derivative, to assign.
+
+        Returns
+        -------
+        ParameterInterface
+            Return self after the value has been set.
         """
         if isinstance(v, ArgumentABC):
             self.value = v.value
@@ -61,31 +66,60 @@ class RecipeOrganizerInterface(object):
     """Mix-in class for enhancing the RecipeOrganizer interface."""
 
     def __imul__(self, args):
-        """Constrain with *=
+        """Constrain with ``*=``.
 
-        Think of '*' as a push-pin.
+        Think of ``*`` as a push-pin. This accepts arguments for a
+        single constraint.
 
-        This accepts arguments for a single constraint.
+        Parameters
+        ----------
+        args : tuple
+            The arguments to pass to `constrain`.
+
+        Returns
+        -------
+        RecipeOrganizerInterface
+            Return self after applying the constraint.
         """
         _applyargs(args, self.constrain)
         return self
 
     def __imod__(self, args):
-        """Restrain with %=
+        """Restrain with ``%=``.
 
-        This of '%' as a loose rope.
+        Think of ``%`` as a loose rope. This accepts arguments for a
+        single restraint.
 
-        This accepts arguments for a single restraint.
+        Parameters
+        ----------
+        args : tuple
+            The arguments to pass to `restrain`.
+
+        Returns
+        -------
+        RecipeOrganizerInterface
+            Return self after applying the restraint.
         """
         _applyargs(args, self.restrain)
         return self
 
     def __iadd__(self, args):
-        """_new_parameter or _add_parameter with +=
+        """Add a parameter with ``+=``.
 
-        Think of "+" as addition of a Parameter.
+        Think of ``+`` as addition of a Parameter. This accepts
+        arguments for a single call to `_new_parameter` or
+        `_add_parameter`.
 
-        This accepts arguments for a single function call.
+        Parameters
+        ----------
+        args : tuple
+            The arguments to pass to `_new_parameter` or
+            `_add_parameter`.
+
+        Returns
+        -------
+        RecipeOrganizerInterface
+            Return self after adding the parameter.
         """
 
         # Want to detect _add_parameter or _new_parameter
@@ -109,22 +143,41 @@ class FitRecipeInterface(object):
     """Mix-in class for enhancing the FitRecipe interface."""
 
     def __ior__(self, args):
-        """AddContribution with |=
+        """Add a contribution with ``|=``.
 
-        Think of "|" as the union of components.
+        Think of ``|`` as the union of components. This accepts a
+        single argument.
 
-        This accepts a single argument.
+        Parameters
+        ----------
+        args : FitContribution
+            The contribution to add.
+
+        Returns
+        -------
+        FitRecipeInterface
+            Return self after adding the contribution.
         """
         self.add_contribution(args)
         return self
 
     def __iadd__(self, args):
-        """add_variable or create_new_variable with +=
+        """Add a variable with ``+=``.
 
-        Think of "+" as addition of a variable.
+        Think of ``+`` as addition of a variable. This accepts a
+        single argument or an iterable of single arguments or
+        argument tuples.
 
-        This accepts a single argument or an iterable of single
-        arguments or argument tuples.
+        Parameters
+        ----------
+        args
+            The arguments to pass to `add_variable` or
+            `create_new_variable`.
+
+        Returns
+        -------
+        FitRecipeInterface
+            Return self after adding the variable.
         """
 
         # Want to detect add_variable or create_new_variable
